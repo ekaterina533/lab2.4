e539a08d682304263ef55d54a247b0643632c522
ekaterina533 <ekaterinahrcnk@gmail.com>
2021-10-26 11:06:48 +0300

    содание 1 задания

---
+++ задание1.py
@@ -0,0 +1,20 @@
+# Вариант 9 вычислить число сочетаний с помощью рекурсии
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def rec(n, m):
+     if n < 0 and m < 0:
+         print("Отрицательные аргументы недопустимы")
+     elif n < m:
+         return 0
+     elif n == m or m == 0:
+         return 1
+     else:
+         return rec(n - 1, m) + rec(n - 1, m - 1)
+
+
+if __name__ == '__main__':
+  n = int(input("Введите целое число"))
+  m = int(input("Введите целое число"))
+  print(rec(n, m))
